3d35df8d105d4634d82e2cf25e382b299467179c
Nick Heisler <nheisler@iastate.edu>
2020-02-23 11:46:21 -0600

    added function methods

---
+++ HW2/src/java/GetLocalTime.java
@@ -0,0 +1,14 @@
+package java;
+
+public class GetLocalTime
+{
+    c_int time;
+    c_char valid;
+
+    public GetLocalTime() { }
+
+    public void execute(String IP, int Port)
+    {
+        // execute the command on the target machine
+    }
+}
