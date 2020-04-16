989e612fe5037c97c641f7b0c2c028b5d6bb6157
Nick Heisler <nheisler@iastate.edu>
2020-04-16 14:58:23 -0500

    working on serialization

--- HW4/java/Header.java
+++ HW4/java/Header.java
@@ -7,4 +7,47 @@ public class Header
     int TTL;
     int Hops;
     int pl_length;
+
+    public Header() { }
+
+    public Header(String ID, int pl_descriptor, int TTL, int Hops, int pl_length)
+    {
+        this.ID = ID;
+        this.pl_descriptor = pl_descriptor;
+        this.TTL = TTL;
+        this.Hops = Hops;
+        this.pl_length = pl_length;
+    }
+
+    public byte[] serialize()
+    {
+        byte[] serial = new byte[23];
+        byte[] ID_byte = ID.getBytes();
+        byte[] pl_length_byte = Macro.toBytes(pl_length);
+
+        for (int i = 0; i < ID_byte.length; i++)
+        {
+            serial[i] = ID_byte[i];
+        }
+        
+        serial[16] = (byte) pl_descriptor;
+        serial[17] = (byte) TTL;
+        serial[18] = (byte) Hops;
+
+        for (int i = 0; i < pl_length_byte.length; i++)
+        {
+            serial[i+18] = pl_length_byte[i];
+        }
+
+        return serial;
+    }
+
+    public static Header deserialize(byte[] data)
+    {
+        Header in = new Header();
+
+
+
+        return in;
+    }
 }
--- HW4/java/Macro.java
+++ HW4/java/Macro.java
@@ -1,5 +1,8 @@
 package java;
 
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+
 public class Macro
 {
     public static int PING = 0x00;
@@ -7,4 +10,14 @@ public class Macro
     public static int PUSH = 0x40;
     public static int QUERY = 0x80;
     public static int QUERYHIT = 0x81;
+
+    public static int toInteger(byte[] bytes)
+    {
+        return ByteBuffer.wrap(bytes).getInt();
+    }
+
+    public static byte[] toBytes(int n)
+    {
+        return ByteBuffer.allocate(4).order(ByteOrder.BIG_ENDIAN).putInt(n).array();
+    }
 }
